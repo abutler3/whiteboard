47a690188c82aebfeb1c200d4cd7e04e33e326d4
Drew Butler <abutler3@gmail.com>
2015-06-21 16:00:30 -0500

    Update projects

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Editing Projects" do\
+
+  before do
+    FactoryGirl.create(:project, name: "Home page redesign")
+    visit "/"
+    click_link "Home page redesign"
+    click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
+    fill_in "Name", with: ".com Redesign beta"
+    click_button "Update Project"
+    expect(page).to have_content("Project has been updated.")
+  end
+
+  scenario "Updating a project with invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has not been updated.")
+  end
+
+end
