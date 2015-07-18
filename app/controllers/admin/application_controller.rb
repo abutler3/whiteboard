1df32696ab2ee48fc81c811913bbd3ea92ddfa1d
Drew Butler <abutler3@gmail.com>
2015-07-18 08:39:00 -0500

    Add admin namespace with application controller

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,16 @@
+class Admin::ApplicationController < ApplicationController
+  before_action :authorize_admin!
+  
+  def index
+  end
+
+  private
+  def authorize_admin!
+    # Devise method to ensure user is signed in
+    authenticate_user!
+
+    unless current_user.admin?
+      redirect_to root_path, alert: "You must be an admin to do that."
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    # get 'application/index'
+    root "application#index"
+  end
+
   devise_for :users
   root "projects#index"
 
