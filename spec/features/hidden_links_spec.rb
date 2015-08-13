7c28765f04e03d2972151aa0bc7b1e06975ce32d
Drew Butler <abutler3@gmail.com>
2015-08-12 19:16:09 -0500

    Hide New Ticket link when user can't update tickets on the project

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,15 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "Users can only see the appropriate links" do
-  # Create a non-admin user
-  let!(:user) { FactoryGirl.create(:user) }
-  # Create a admin user
+RSpec.feature "Users can only see the appropriate links" do
+  let(:project) { FactoryGirl.create(:project) }
+  let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:user, :admin) }
-  let!(:project) { FactoryGirl.create(:project) }
 
   context "anonymous users" do
     scenario "cannot see the New Project link" do
-      visit '/'
+      visit "/"
       expect(page).not_to have_link "New Project"
     end
   end
@@ -21,10 +19,13 @@ describe "Users can only see the appropriate links" do
     end
 
     scenario "cannot see the New Project link" do
-      visit '/'
+      visit "/"
       expect(page).not_to have_link "New Project"
     end
-
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete Project"
+    end
     scenario "cannot see the Edit Project link" do
       visit project_path(project)
       expect(page).not_to have_link "Edit Project"
@@ -35,38 +36,16 @@ describe "Users can only see the appropriate links" do
     before { login_as(admin) }
 
     scenario "can see the New Project link" do
-      visit '/'
+      visit "/"
       expect(page).to have_link "New Project"
     end
-
+    scenario "can see the Delete Project link" do
+      visit project_path(project)
+      expect(page).to have_link "Delete Project"
+    end
     scenario "can see the Edit Project link" do
       visit project_path(project)
       expect(page).to have_link "Edit Project"
-    end
-  end
-
-  context "anonymous users" do
-    scenario "cannot see the New Project link" do
-      visit project_path(project)
-      expect(page).not_to have_link "Delete Project"
-    end
-  end
-
-  context "regular users" do
-    before { login_as(user) }
-
-    scenario "cannot see the New Project link" do
-      visit project_path(project)
-      expect(page).not_to have_link "Delete Project"
-    end
-  end
-
-  context "admin users" do
-    before { login_as(admin) }
-
-    scenario "can see the New Project link" do
-      visit project_path(project)
-      expect(page).to have_link "Delete Project"
     end
   end
 end
